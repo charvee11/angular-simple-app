bcd633a9fb2c8bc39f23f5c502d4aa59f784e9f7
charvee11 <charvee.work@gmail.com>
2019-04-30 15:55:31 +0530

    added npm install

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 echo Build started on `date`
 echo Compiling the Node.js code
-npm i typescript@3.1.6
+
 npm install --save core-js@^2.5.0
+npm install typescript@'>=2.4.2 <2.5.0'
 npm install @angular-devkit/core
 npm install node-sass
 npm install
